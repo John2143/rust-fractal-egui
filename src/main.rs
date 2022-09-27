ef8471e2f3a8f1ecfc94b72eb3a2e81a2f85c900
John Schmidt <john@john2143.com>
2022-09-27 00:27:31 -0400

    function views

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use eframe::egui;
 use egui::{Color32, ColorImage, Rgba, TextureHandle, TextureId, Ui};
+use itertools::Itertools;
 use num_traits::{Float, Num, Zero};
+use rayon::prelude::*;
 
 #[derive(Clone, Copy, Debug)]
 struct Complex<T> {
@@ -51,12 +53,12 @@ enum IterResult<T> {
     Inside(T),
 }
 
+const UNROLL: usize = 4;
+
 fn iterate<T>(c: Complex<T>, times: usize) -> IterResult<Complex<T>>
 where
     T: Num + Copy + PartialOrd,
 {
-    const UNROLL: usize = 8;
-
     let mut z = c.clone();
     for i in 0..(times / UNROLL) {
         for _ in 0..UNROLL {
@@ -64,41 +66,28 @@ where
             z = z * z + c;
         }
         if z.len_sq() > (T::one() + T::one()) {
-            return IterResult::Outside(i * UNROLL);
+            return IterResult::Outside(i);
         }
     }
 
     IterResult::Inside(z)
 }
 
-fn old_main() {
-    for y in (0..50).map(|i| -1.0 + 2.0 * (i as f64) / 50.0) {
-        for x in (0..80).map(|i| -1.5 + 2.5 * (i as f64) / 80.0) {
-            match iterate(Complex::new(x, y), 100) {
-                IterResult::Outside(_) => print!("."),
-                IterResult::Inside(_) => print!("#"),
-            }
-        }
-        println!();
-    }
-
-    println!("Hello, world!");
-}
-
 fn main() {
     let options = eframe::NativeOptions::default();
     eframe::run_native(
-        "My egui App",
+        "Fractal Renderer",
         options,
         Box::new(|_cc| Box::new(MyApp::default())),
     );
 }
 
 struct MyApp {
-    name: String,
-    age: u32,
+    steps: usize,
     image_texture: Option<TextureHandle>,
     next_frame: Option<ColorImage>,
+    top_left: Complex<FL>,
+    bottom_right: Complex<FL>,
 }
 
 impl MyApp {
@@ -116,10 +105,12 @@ impl MyApp {
     }
 }
 
-fn get_xy(c: Complex<FL>) -> Rgba {
+fn get_xy(c: Complex<FL>, step_max: usize) -> Rgba {
     //dbg!(&c);
-    match iterate(c, 100) {
-        IterResult::Outside(_steps) => Rgba::from_rgba_unmultiplied(0.0, 1.0, 1.0, 1.0),
+    match iterate(c, step_max) {
+        IterResult::Outside(steps) => {
+            Rgba::from_rgba_unmultiplied(0.0, steps as f32 / step_max as f32, 1.0, 1.0)
+        }
         IterResult::Inside(_final) => Rgba::from_rgba_unmultiplied(1.0, 1.0, 1.0, 1.0),
     }
 }
@@ -130,29 +121,33 @@ fn generate_view(
     [x_size, y_size]: [usize; 2],
     top_left: Complex<FL>,
     bot_right: Complex<FL>,
+    steps: usize,
 ) -> ColorImage {
-    let mut i = ColorImage::new([x_size, y_size], Color32::BLACK);
-
-    for y in 0..y_size {
-        for x in 0..x_size {
+    let pixels: Vec<u8> = (0..y_size)
+        .cartesian_product(0..x_size)
+        .map(|(y, x)| {
             let cx = top_left.r + (x as FL / x_size as FL) * (bot_right.r - top_left.r);
             let cy = top_left.i - (y as FL / y_size as FL) * (top_left.i - bot_right.i);
 
-            let c = get_xy(Complex::new(cx, cy));
-            i.pixels[x + y * y_size] = c.into();
-        }
-    }
+            let c = get_xy(Complex::new(cx, cy), steps);
+            c.to_srgba_unmultiplied()
+        })
+        .flatten()
+        .collect();
 
-    i
+    ColorImage::from_rgba_unmultiplied([x_size, y_size], &pixels)
 }
 
 impl Default for MyApp {
     fn default() -> Self {
-        let view = generate_view([400, 400], Complex::new(-2.0, 1.0), Complex::new(1.0, -1.0));
+        let top_left = Complex::new(-2.0, 1.0);
+        let bottom_right = Complex::new(1.0, -1.0);
+        let view = generate_view([400, 400], top_left, bottom_right, 10);
 
         Self {
-            name: "Arthur".to_owned(),
-            age: 42,
+            top_left,
+            bottom_right,
+            steps: 10,
             next_frame: Some(view),
             image_texture: None,
         }
@@ -164,16 +159,23 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             self.set_image(ui);
 
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
+            //ui.heading("My egui Application");
+            //ui.horizontal(|ui| {
+            //ui.label("Your name: ");
+            //ui.text_edit_singleline(&mut self.name);
+            //});
+            ui.add(egui::Slider::new(&mut self.steps, UNROLL..=100).text("Steps"));
+
+            if ui.button("Regen").clicked() {
+                self.next_frame = Some(generate_view(
+                    [400, 400],
+                    self.top_left,
+                    self.bottom_right,
+                    self.steps,
+                ));
             }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+            ui.label(format!("Steps: {}", self.steps));
+
             let t = self.image_texture.as_ref().unwrap();
             ui.image(t, t.size_vec2());
             println!("yep");
