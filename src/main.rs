ae584aae1c80fce13a854a2cd62780031a7d1714
John Schmidt <john@john2143.com>
2022-09-27 01:14:27 -0400

    full movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::ops::AddAssign;
+
 use eframe::egui;
-use egui::{ColorImage, Rgba, TextureHandle, Ui};
+use egui::{ColorImage, Event, Rgba, TextureHandle, Ui};
 use num_traits::Num;
 use rayon::prelude::*;
 
@@ -30,6 +32,24 @@ impl<T: Num> std::ops::Add<Complex<T>> for Complex<T> {
     }
 }
 
+impl<T: Num> std::ops::Sub<Complex<T>> for Complex<T> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self {
+            r: self.r - rhs.r,
+            i: self.i - rhs.i,
+        }
+    }
+}
+
+impl<T: Num + AddAssign> std::ops::AddAssign<Complex<T>> for Complex<T> {
+    fn add_assign(&mut self, rhs: Complex<T>) {
+        self.r += rhs.r;
+        self.i += rhs.i;
+    }
+}
+
 impl<T: Num + Copy> std::ops::Mul<Complex<T>> for Complex<T> {
     type Output = Self;
 
@@ -42,6 +62,17 @@ impl<T: Num + Copy> std::ops::Mul<Complex<T>> for Complex<T> {
         Self {
             r: self.r * rhs.r - self.i * rhs.i,
             i: self.r * rhs.i + self.i * rhs.r,
+        }
+    }
+}
+
+impl<T: Num + Copy> std::ops::Mul<T> for Complex<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self {
+        Self {
+            r: self.r * rhs,
+            i: self.i * rhs,
         }
     }
 }
@@ -111,6 +142,24 @@ impl ViewData {
 
         ColorImage::from_rgba_unmultiplied([x_size, y_size], &pixels)
     }
+
+    fn pan(&mut self, change: Complex<FL>) {
+        self.top_left += change;
+        self.bottom_right += change;
+    }
+
+    fn rel_move(&mut self, [x, y]: [i8; 2]) {
+        let movespeed = self.bottom_right.r - self.top_left.r;
+        let d = Complex::new(x as FL, y as FL) * movespeed * 0.5;
+        self.pan(d);
+    }
+
+    fn rel_zoom(&mut self, x: i8) {
+        let delta = self.bottom_right - self.top_left;
+
+        self.top_left += delta * 0.25 * x as FL;
+        self.bottom_right += delta * -0.25 * x as FL;
+    }
 }
 
 struct MyApp {
@@ -168,6 +217,62 @@ impl Default for MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        for event in &ctx.input().events {
+            match event {
+                Event::Key {
+                    key,
+                    pressed, ..
+                    //modifiers,
+                } => {
+                    println!("pressed {:?} {pressed}", key);
+                    if !pressed { return }
+
+                    use egui::Key::*;
+                    let redraw = match key {
+                        ArrowDown => {self.view_data.rel_move([0, -1]); true},
+                        ArrowUp => {self.view_data.rel_move([0, 1]); true}
+                        ArrowLeft => {self.view_data.rel_move([-1, 0]); true}
+                        ArrowRight => {self.view_data.rel_move([1, 0]); true}
+                        Q => {self.view_data.rel_zoom(1); true}
+                        E => {self.view_data.rel_zoom(-1); true}
+                        Escape => todo!(),
+                        Tab => todo!(),
+                        Backspace => todo!(),
+                        Enter => todo!(),
+                        Space => todo!(),
+                        Insert => todo!(),
+                        Delete => todo!(),
+                        _ => false,
+                    };
+
+                    if redraw {
+                        self.next_frame = Some(self.view_data.generate_view([600, 400]));
+                    }
+                }
+                //Event::Paste(_) => todo!(),
+                //Event::Text(_) => todo!(),
+                //Event::PointerMoved(_) => todo!(),
+                //Event::PointerButton {
+                //pos,
+                //button,
+                //pressed,
+                //modifiers,
+                //} => todo!(),
+                //Event::PointerGone => todo!(),
+                //Event::Scroll(_) => todo!(),
+                //Event::CompositionStart => todo!(),
+                //Event::CompositionUpdate(_) => todo!(),
+                //Event::CompositionEnd(_) => todo!(),
+                //Event::Touch {
+                //device_id,
+                //id,
+                //phase,
+                //pos,
+                //force,
+                //} => todo!(),
+                _ => {}
+            }
+        }
         egui::CentralPanel::default().show(ctx, |ui| {
             self.set_image(ui);
 
